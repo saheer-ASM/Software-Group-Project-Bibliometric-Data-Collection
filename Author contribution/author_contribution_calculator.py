e8a6080de12b906aa2222a158d91b97b5583cd7c
Shaith-Ahamed <shaith1208@gmail.com>
2026-08-12 17:36:56 +0530

    the author contribution code edited to get specfic list of authors weight

--- Author contribution/author_contribution_calculator.py
+++ Author contribution/author_contribution_calculator.py
@@ -1,62 +1,250 @@
+# import math
+# from typing import List, Dict, Optional
+
+
+# class AuthorContributionCalculator:
+#     """
+#     A class to calculate author contributions based on different ordering norms.
+#     Supports Alphabetical/Random and Relative Contribution ordering.
+#     """
+
+#     def __init__(self):
+#         self.field_weights = {}
+#         self.author_positions = {}
+
+#     def detect_ordering_norm(
+#             self,
+#             author_list: List[str],
+#             paper_id: str
+#     ) -> str:
+
+#         sorted_authors = sorted(author_list)
+
+
+#         if [a.lower() for a in author_list] == \
+#            [a.lower() for a in sorted_authors]:
+
+#             return "alphabetical"
+
+#         else:
+
+#             return "relative"
+
+
+#     def calculate_alphabetical_weights(
+#             self,
+#             paper_id: str,
+#             field_name: str,
+#             field_weight: float,
+#             num_authors: int
+#     ) -> Dict[int,float]:
+
+
+#         if num_authors == 0:
+#             return {}
+
+
+#         contribution = field_weight / num_authors
+
+
+#         weights={}
+
+
+#         for i in range(1,num_authors+1):
+
+#             weights[i]=contribution
+
+
+#         return weights
+
+
+
+#     def calculate_relative_weights(
+#             self,
+#             paper_id: str,
+#             field_name: str,
+#             field_weight: float,
+#             num_authors: int,
+#             core_first_indices=None,
+#             core_last_indices=None
+#     ):
+
+
+#         if num_authors==0:
+#             return {}
+
+
+#         core_first = core_first_indices or []
+#         core_last = core_last_indices or []
+
+
+#         eap_values={}
+
+
+#         for idx in range(1,num_authors+1):
+
+#             if idx in core_first:
+
+#                 eap_values[idx]=sum(core_first)/len(core_first)
+
+
+#             elif idx in core_last:
+
+#                 eap_values[idx]=sum(core_last)/len(core_last)
+
+
+#             else:
+
+#                 eap_values[idx]=idx
+
+
+
+#         harmonic_weights={}
+
+
+#         for idx,eap in eap_values.items():
+
+#             harmonic_weights[idx]=1/eap
+
+
+
+#         total=sum(harmonic_weights.values())
+
+
+#         weights={}
+
+
+#         for idx,value in harmonic_weights.items():
+
+#             weights[idx]=(field_weight*value)/total
+
+
+#         return weights
+
+
+
+
+
+#     def calculate_total_author_contributions(
+#             self,
+#             paper_id,
+#             author_list,
+#             fields_data,
+#             core_first_indices=None,
+#             core_last_indices=None
+#     ):
+
+
+#         num_authors=len(author_list)
+
+
+#         ordering_norm=self.detect_ordering_norm(
+#             author_list,
+#             paper_id
+#         )
+
+
+#         author_contributions={
+
+#             author:0.0
+
+#             for author in author_list
+
+#         }
+
+
+#         detailed={}
+
+
+
+#         for field,weight in fields_data.items():
+
+
+#             if ordering_norm=="alphabetical":
+
+
+#                 result=self.calculate_alphabetical_weights(
+
+#                     paper_id,
+#                     field,
+#                     weight,
+#                     num_authors
+
+#                 )
+
+
+#             else:
+
+
+#                 result=self.calculate_relative_weights(
+
+#                     paper_id,
+#                     field,
+#                     weight,
+#                     num_authors,
+#                     core_first_indices,
+#                     core_last_indices
+
+#                 )
+
+
+
+#             detailed[field]={}
+
+
+
+#             for index,value in result.items():
+
+#                 author=author_list[index-1]
+
+
+#                 detailed[field][author]=value
+
+
+#                 author_contributions[author]+=value
+
+
+
+
+
+#         return {
+
+
+#             "ordering_norm":ordering_norm,
+
+#             "field_weights":fields_data,
+
+#             "author_contributions":author_contributions,
+
+#             "detailed_contributions":detailed,
+
+#             "total_weight":sum(
+#                 author_contributions.values()
+#             )
+
+#         }
+
+
+
+
+
+
+
+
+
 import math
-from typing import List, Dict, Optional
+from typing import List, Dict
 
 
 class AuthorContributionCalculator:
     """
-    A class to calculate author contributions based on different ordering norms.
-    Supports Alphabetical/Random and Relative Contribution ordering.
+    Calculate author contributions assuming all papers follow
+    relative contribution ordering.
     """
 
     def __init__(self):
         self.field_weights = {}
         self.author_positions = {}
-
-    def detect_ordering_norm(
-            self,
-            author_list: List[str],
-            paper_id: str
-    ) -> str:
-
-        sorted_authors = sorted(author_list)
-
-
-        if [a.lower() for a in author_list] == \
-           [a.lower() for a in sorted_authors]:
-
-            return "alphabetical"
-
-        else:
-
-            return "relative"
-
-
-    def calculate_alphabetical_weights(
-            self,
-            paper_id: str,
-            field_name: str,
-            field_weight: float,
-            num_authors: int
-    ) -> Dict[int,float]:
-
-
-        if num_authors == 0:
-            return {}
-
-
-        contribution = field_weight / num_authors
-
-
-        weights={}
-
-
-        for i in range(1,num_authors+1):
-
-            weights[i]=contribution
-
-
-        return weights
-
 
 
     def calculate_relative_weights(
@@ -69,8 +257,7 @@ class AuthorContributionCalculator:
             core_last_indices=None
     ):
 
-
-        if num_authors==0:
+        if num_authors == 0:
             return {}
 
 
@@ -78,50 +265,56 @@ class AuthorContributionCalculator:
         core_last = core_last_indices or []
 
 
-        eap_values={}
+        eap_values = {}
 
 
-        for idx in range(1,num_authors+1):
+        for idx in range(1, num_authors + 1):
 
             if idx in core_first:
 
-                eap_values[idx]=sum(core_first)/len(core_first)
+                eap_values[idx] = (
+                    sum(core_first) / len(core_first)
+                )
 
 
             elif idx in core_last:
 
-                eap_values[idx]=sum(core_last)/len(core_last)
+                eap_values[idx] = (
+                    sum(core_last) / len(core_last)
+                )
 
 
             else:
 
-                eap_values[idx]=idx
+                eap_values[idx] = idx
 
 
 
-        harmonic_weights={}
+        # Harmonic weighting
+        harmonic_weights = {}
+
+        for idx, eap in eap_values.items():
+
+            harmonic_weights[idx] = 1 / eap
 
 
-        for idx,eap in eap_values.items():
 
-            harmonic_weights[idx]=1/eap
-
-
-
-        total=sum(harmonic_weights.values())
+        total = sum(
+            harmonic_weights.values()
+        )
 
 
-        weights={}
+        weights = {}
 
 
-        for idx,value in harmonic_weights.items():
+        for idx, value in harmonic_weights.items():
 
-            weights[idx]=(field_weight*value)/total
+            weights[idx] = (
+                field_weight * value
+            ) / total
 
 
         return weights
-
-
 
 
 
@@ -135,90 +328,80 @@ class AuthorContributionCalculator:
     ):
 
 
-        num_authors=len(author_list)
+        num_authors = len(author_list)
 
 
-        ordering_norm=self.detect_ordering_norm(
-            author_list,
-            paper_id
-        )
+        # Assume every paper follows relative ordering
+        ordering_norm = "relative"
 
 
-        author_contributions={
 
-            author:0.0
+        author_contributions = {
+
+            author: 0.0
 
             for author in author_list
 
         }
 
 
-        detailed={}
+        detailed = {}
 
 
 
-        for field,weight in fields_data.items():
+        for field, weight in fields_data.items():
 
 
-            if ordering_norm=="alphabetical":
+            result = self.calculate_relative_weights(
+
+                paper_id,
+
+                field,
+
+                weight,
+
+                num_authors,
+
+                core_first_indices,
+
+                core_last_indices
+
+            )
 
 
-                result=self.calculate_alphabetical_weights(
-
-                    paper_id,
-                    field,
-                    weight,
-                    num_authors
-
-                )
-
-
-            else:
-
-
-                result=self.calculate_relative_weights(
-
-                    paper_id,
-                    field,
-                    weight,
-                    num_authors,
-                    core_first_indices,
-                    core_last_indices
-
-                )
+            detailed[field] = {}
 
 
 
-            detailed[field]={}
+            for index, value in result.items():
 
 
-
-            for index,value in result.items():
-
-                author=author_list[index-1]
+                author = author_list[index - 1]
 
 
-                detailed[field][author]=value
+                detailed[field][author] = value
 
 
-                author_contributions[author]+=value
-
-
+                author_contributions[author] += value
 
 
 
         return {
 
 
-            "ordering_norm":ordering_norm,
+            "ordering_norm": ordering_norm,
 
-            "field_weights":fields_data,
 
-            "author_contributions":author_contributions,
+            "field_weights": fields_data,
 
-            "detailed_contributions":detailed,
 
-            "total_weight":sum(
+            "author_contributions": author_contributions,
+
+
+            "detailed_contributions": detailed,
+
+
+            "total_weight": sum(
                 author_contributions.values()
             )
 
--- Author contribution/main.py
+++ Author contribution/main.py
@@ -1,15 +1,458 @@
+# from database import get_connection
+# from author_contribution_calculator import AuthorContributionCalculator
+
+
+
+# def get_publications():
+
+#     conn = get_connection()
+#     cur = conn.cursor()
+
+#     query = """
+#         SELECT 
+#             pub_id,
+#             author1id,
+#             author2id,
+#             author3id,
+#             author4id,
+#             author5id,
+#             author6id,
+#             author7id,
+#             author8id,
+#             author9id,
+#             author10id
+#         FROM public.author_contribution_weight
+#     """
+
+#     cur.execute(query)
+
+#     rows = cur.fetchall()
+
+#     cur.close()
+#     conn.close()
+
+#     return rows
+
+
+
+
+
+# def get_author_name(author_id):
+
+#     if author_id is None:
+#         return None
+
+
+#     conn = get_connection()
+#     cur = conn.cursor()
+
+
+#     query = """
+#         SELECT author_name
+#         FROM public.author
+#         WHERE author_id=%s
+#     """
+
+
+#     cur.execute(query, (author_id,))
+
+
+#     result = cur.fetchone()
+
+
+#     cur.close()
+#     conn.close()
+
+
+#     if result:
+#         return result[0]
+
+#     return None
+
+
+
+
+
+# def get_field_weights(pub_id):
+
+#     conn = get_connection()
+#     cur = conn.cursor()
+
+
+#     query = """
+#         SELECT
+
+#         field1_name,
+#         field1_weight,
+
+#         field2_name,
+#         field2_weight,
+
+#         field3_name,
+#         field3_weight
+
+#         FROM public.field_classification
+
+#         WHERE pub_id=%s
+#     """
+
+
+#     cur.execute(query, (pub_id,))
+
+
+#     row = cur.fetchone()
+
+
+#     cur.close()
+#     conn.close()
+
+
+
+#     fields = {}
+
+
+#     if row:
+
+
+#         if row[0] and row[1] is not None:
+
+#             fields[row[0]] = float(row[1])
+
+
+#         if row[2] and row[3] is not None:
+
+#             fields[row[2]] = float(row[3])
+
+
+#         if row[4] and row[5] is not None:
+
+#             fields[row[4]] = float(row[5])
+
+
+
+#     return fields
+
+
+
+
+
+
+# def update_author_weights(pub_id, result, author_ids):
+
+
+#     conn = get_connection()
+
+#     cur = conn.cursor()
+
+
+
+#     weights = []
+
+
+#     # Calculate weight for existing authors
+
+#     for author_id in author_ids:
+
+
+#         author_name = get_author_name(author_id)
+
+
+#         contribution = result["author_contributions"].get(
+#             author_name,
+#             0
+#         )
+
+
+#         weights.append(contribution)
+
+
+
+#     # Padding remaining author positions up to 10
+
+#     while len(weights) < 10:
+
+#         weights.append(None)
+
+
+
+
+#     query = """
+
+#     UPDATE public.author_contribution_weight
+
+#     SET
+
+#         author_ordering_norm=%s,
+
+#         author1id_weight=%s,
+#         author2id_weight=%s,
+#         author3id_weight=%s,
+#         author4id_weight=%s,
+#         author5id_weight=%s,
+#         author6id_weight=%s,
+#         author7id_weight=%s,
+#         author8id_weight=%s,
+#         author9id_weight=%s,
+#         author10id_weight=%s
+
+
+#     WHERE pub_id=%s
+
+#     """
+
+
+
+#     cur.execute(
+
+#         query,
+
+#         (
+
+#             result["ordering_norm"],
+
+#             weights[0],
+#             weights[1],
+#             weights[2],
+#             weights[3],
+#             weights[4],
+#             weights[5],
+#             weights[6],
+#             weights[7],
+#             weights[8],
+#             weights[9],
+
+#             pub_id
+
+#         )
+
+#     )
+
+
+
+#     conn.commit()
+
+
+#     cur.close()
+
+#     conn.close()
+
+
+
+
+
+
+
+# def process_publications():
+
+
+#     calculator = AuthorContributionCalculator()
+
+
+#     publications = get_publications()
+
+
+
+#     for row in publications:
+
+
+
+#         pub_id = row[0]
+
+
+#         print("\n==============================")
+#         print("Processing:", pub_id)
+#         print("==============================")
+
+
+
+#         # Extract author IDs
+
+#         author_ids = list(row[1:])
+
+
+
+#         # Remove NULL author IDs
+
+#         author_ids = [
+
+#             author_id
+
+#             for author_id in author_ids
+
+#             if author_id is not None
+
+#         ]
+
+
+
+#         if len(author_ids) == 0:
+
+#             print("No authors found")
+
+#             continue
+
+
+
+
+#         # Get author names
+
+#         author_names = []
+
+
+
+#         for author_id in author_ids:
+
+
+#             name = get_author_name(author_id)
+
+
+#             if name:
+
+#                 author_names.append(name)
+
+
+
+#         print("Authors:")
+#         for i,name in enumerate(author_names,1):
+
+#             print(i,name)
+
+
+
+
+#         # Get field weights
+
+#         fields = get_field_weights(pub_id)
+
+
+
+#         print("\nFields:")
+#         print(fields)
+
+
+
+#         if len(fields)==0:
+
+#             print("No field classification found")
+
+#             continue
+
+
+
+
+#         # Calculate contribution
+
+
+#         result = calculator.calculate_total_author_contributions(
+
+#             paper_id=pub_id,
+
+#             author_list=author_names,
+
+#             fields_data=fields
+
+#         )
+
+
+
+#         print("\nOrdering:", result["ordering_norm"])
+
+
+
+#         print("\nContribution:")
+
+#         for author,weight in result["author_contributions"].items():
+
+#             print(
+#                 author,
+#                 "=>",
+#                 round(weight,6)
+#             )
+
+
+
+#         print(
+#             "Total:",
+#             result["total_weight"]
+#         )
+
+
+
+#         # Update database
+
+#         update_author_weights(
+
+#             pub_id,
+
+#             result,
+
+#             author_ids
+
+#         )
+
+
+
+#         print("\nUpdated successfully:", pub_id)
+
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+
+#     process_publications()
+
+
+
+
+
+
+
+
 from database import get_connection
 from author_contribution_calculator import AuthorContributionCalculator
 
 
-
 def get_publications():
+   
 
+    target_pub_ids = [
+
+        "W4319034505",
+        "W4384827634",
+        "W4385491147",
+        "W4386283421",
+        "W4407239538",
+        "W4392896753",
+        "W4393856957",
+        "W4394575343",
+        "W4400471621",
+        "W4206908489",
+        "W4211005424",
+        "W4283171783",
+        "W4288751738",
+        "W4296105462",
+        "W4307377025",
+        "W3203963560",
+        "W2999010100",
+        "W7118860778",
+        "W7133678749",
+        "W7161806135",
+        "W4411278388",
+        "W4394754282"
+
+    ]
+
+  
     conn = get_connection()
     cur = conn.cursor()
 
     query = """
-        SELECT 
+        SELECT
             pub_id,
             author1id,
             author2id,
@@ -22,9 +465,11 @@ def get_publications():
             author9id,
             author10id
         FROM public.author_contribution_weight
+        WHERE pub_id = ANY(%s)
+        ORDER BY pub_id
     """
 
-    cur.execute(query)
+    cur.execute(query, (target_pub_ids,))
 
     rows = cur.fetchall()
 
@@ -34,18 +479,13 @@ def get_publications():
     return rows
 
 
-
-
-
 def get_author_name(author_id):
 
     if author_id is None:
         return None
 
-
     conn = get_connection()
     cur = conn.cursor()
-
 
     query = """
         SELECT author_name
@@ -53,16 +493,12 @@ def get_author_name(author_id):
         WHERE author_id=%s
     """
 
-
     cur.execute(query, (author_id,))
-
 
     result = cur.fetchone()
 
-
     cur.close()
     conn.close()
-
 
     if result:
         return result[0]
@@ -70,14 +506,10 @@ def get_author_name(author_id):
     return None
 
 
-
-
-
 def get_field_weights(pub_id):
 
     conn = get_connection()
     cur = conn.cursor()
-
 
     query = """
         SELECT
@@ -96,85 +528,51 @@ def get_field_weights(pub_id):
         WHERE pub_id=%s
     """
 
-
     cur.execute(query, (pub_id,))
 
-
     row = cur.fetchone()
-
 
     cur.close()
     conn.close()
 
-
-
     fields = {}
-
 
     if row:
 
-
         if row[0] and row[1] is not None:
-
             fields[row[0]] = float(row[1])
 
-
         if row[2] and row[3] is not None:
-
             fields[row[2]] = float(row[3])
 
-
         if row[4] and row[5] is not None:
-
             fields[row[4]] = float(row[5])
-
-
 
     return fields
 
 
-
-
-
-
 def update_author_weights(pub_id, result, author_ids):
 
-
     conn = get_connection()
-
     cur = conn.cursor()
-
-
 
     weights = []
 
-
     # Calculate weight for existing authors
-
     for author_id in author_ids:
 
-
         author_name = get_author_name(author_id)
-
 
         contribution = result["author_contributions"].get(
             author_name,
             0
         )
 
-
         weights.append(contribution)
 
-
-
     # Padding remaining author positions up to 10
-
     while len(weights) < 10:
-
         weights.append(None)
-
-
-
 
     query = """
 
@@ -195,12 +593,9 @@ def update_author_weights(pub_id, result, author_ids):
         author9id_weight=%s,
         author10id_weight=%s
 
-
     WHERE pub_id=%s
 
     """
-
-
 
     cur.execute(
 
@@ -227,63 +622,37 @@ def update_author_weights(pub_id, result, author_ids):
 
     )
 
-
-
     conn.commit()
 
-
     cur.close()
-
     conn.close()
-
-
-
-
-
 
 
 def process_publications():
 
-
     calculator = AuthorContributionCalculator()
-
 
     publications = get_publications()
 
-
+    print(f"\nFound {len(publications)} publications to process.\n")
 
     for row in publications:
 
-
-
         pub_id = row[0]
-
 
         print("\n==============================")
         print("Processing:", pub_id)
         print("==============================")
 
-
-
         # Extract author IDs
-
         author_ids = list(row[1:])
 
-
-
         # Remove NULL author IDs
-
         author_ids = [
-
             author_id
-
             for author_id in author_ids
-
             if author_id is not None
-
         ]
-
-
 
         if len(author_ids) == 0:
 
@@ -291,58 +660,34 @@ def process_publications():
 
             continue
 
-
-
-
         # Get author names
-
         author_names = []
-
-
 
         for author_id in author_ids:
 
-
             name = get_author_name(author_id)
 
-
             if name:
-
                 author_names.append(name)
 
-
-
         print("Authors:")
-        for i,name in enumerate(author_names,1):
 
-            print(i,name)
-
-
-
+        for i, name in enumerate(author_names, 1):
+            print(i, name)
 
         # Get field weights
-
         fields = get_field_weights(pub_id)
-
-
 
         print("\nFields:")
         print(fields)
 
-
-
-        if len(fields)==0:
+        if len(fields) == 0:
 
             print("No field classification found")
 
             continue
 
-
-
-
         # Calculate contribution
-
-
         result = calculator.calculate_total_author_contributions(
 
             paper_id=pub_id,
@@ -353,33 +698,24 @@ def process_publications():
 
         )
 
-
-
         print("\nOrdering:", result["ordering_norm"])
-
-
 
         print("\nContribution:")
 
-        for author,weight in result["author_contributions"].items():
+        for author, weight in result["author_contributions"].items():
 
             print(
                 author,
                 "=>",
-                round(weight,6)
+                round(weight, 6)
             )
-
-
 
         print(
             "Total:",
             result["total_weight"]
         )
 
-
-
         # Update database
-
         update_author_weights(
 
             pub_id,
@@ -390,18 +726,15 @@ def process_publications():
 
         )
 
-
-
         print("\nUpdated successfully:", pub_id)
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
 
     process_publications()
+
+
+
+
+
+
